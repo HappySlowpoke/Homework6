81e2073ca58126230e72866c030a39d235dc4ce7
sofiiakostrubina <slowpokehappy@gmail.com>
2025-01-26 23:11:17 +0200

    pr

--- src/User.ts
+++ src/User.ts
@@ -1,15 +1,22 @@
 export class User {
-    name:string
-    surname: string
-    phoneNumber: string
-    address: string
-    consentGiven: boolean | undefined
+  name: string;
+  surname: string;
+  phoneNumber: string;
+  address: string;
+  age: number;
+  consentGiven: boolean | undefined;
 
-    constructor(name:string, surname: string, phoneNumber: string, address: string) {
-    this.name = name
-        this.address = address
-        this.surname = surname
-        this.phoneNumber = phoneNumber
-
-    }
+  constructor(
+    name: string,
+    surname: string,
+    phoneNumber: string,
+    address: string,
+    age: number,
+  ) {
+    this.name = name;
+    this.address = address;
+    this.surname = surname;
+    this.phoneNumber = phoneNumber;
+    this.age = age;
+  }
 }
--- test/Processor.test.ts
+++ test/Processor.test.ts
@@ -1,28 +1,42 @@
-import {User} from "../src/User";
-import {Processor} from "../src/Processor";
+import { User } from "../src/User";
+import { Processor } from "../src/Processor";
 
 describe("Processor test", () => {
-    let user:User
-    beforeEach(() => {
-        user = new User('Albert', 'Einstein', '123456789', 'SouthPole 28')
-    })
+  let user: User;
+  let user2: User;
+  beforeEach(() => {
+    user = new User("Albert", "Einstein", "123456789", "SouthPole 28", 16);
+    user2 = new User("Vasya", "Pupkin", "9876543210", "SouthPole 28", 25);
+  });
 
-    test("Default state undefined", () => {
-        expect(user.consentGiven).toBeUndefined()
-    })
+  test("Default state undefined", () => {
+    expect(user.consentGiven).toBeUndefined();
+  });
 
-    test("State is true", () => {
-        Processor.giveConsent(user)
-        expect(user.consentGiven).toBe(true)
-    })
+  test("State is true", () => {
+    Processor.giveConsent(user);
+    expect(user.consentGiven).toBe(true);
+  });
 
-    test("Processor verify consent", () => {
-        Processor.giveConsent(user)
-        expect(Processor.verifyConsent(user)).toBeTruthy()
-    })
+  test("Processor verify consent", () => {
+    Processor.giveConsent(user);
+    expect(Processor.verifyConsent(user)).toBeTruthy();
+  });
 
-    test("Verify consent is false", () => {
-        expect(Processor.verifyConsent(user)).toBeFalsy()
-    })
-})
+  test("Verify consent is false", () => {
+    expect(Processor.verifyConsent(user)).toBeFalsy();
+  });
 
+  test("Verify check consent eligibility under age", () => {
+    expect(Processor.checkConsentEligibility(user.age)).toBeFalsy();
+  });
+
+  test("Verify check consent eligibility over age", () => {
+    expect(Processor.checkConsentEligibility(user2.age)).toBeTruthy();
+  });
+
+  test("Revoke consent works correctly", () => {
+    expect(Processor.revokeConsent(user2)).toBeFalsy();
+  });
+
+});
